9122cccc8f75ab13e928f4e411115ec8ba57f80f
Олександр <pasash07@gmail.com>
2024-10-24 14:02:28 +0300

    Update steck3.6.cpp

--- steck3.6.cpp
+++ steck3.6.cpp
@@ -1,12 +1,62 @@
 #include "steck3.6.h"
 #include <iostream>
 
+using namespace std;
+ 
 /*
-* Реалізувати програму для роботи з лінійним списком рядків. Передбачити функції
-додавання, видалення та читання даних зі списку.
+* Р РµР°Р»С–Р·СѓРІР°С‚Рё РїСЂРѕРіСЂР°РјСѓ РґР»СЏ СЂРѕР±РѕС‚Рё Р· Р»С–РЅС–Р№РЅРёРј СЃРїРёСЃРєРѕРј СЂСЏРґРєС–РІ. РџРµСЂРµРґР±Р°С‡РёС‚Рё С„СѓРЅРєС†С–С—
+РґРѕРґР°РІР°РЅРЅСЏ, РІРёРґР°Р»РµРЅРЅСЏ С‚Р° С‡РёС‚Р°РЅРЅСЏ РґР°РЅРёС… Р·С– СЃРїРёСЃРєСѓ.
 */
 
-void Push(char element)
+//private section in cpp
+const int SIZE = 150;
+
+string mASSiv [SIZE];
+int top = 0;
+
+
+// base function
+void Push(string elem)
 {
+	if (!isFull())
+	{
+		mASSiv[top] = elem; top++;
+	}
+	else
+	{
+		cout << "РќРµРІРёСЃС‚Р°С‡Р°С” СЂСЏРґРєС–РІ\n";
+	}
+}
+string Pop()
+{
+	if (!isEmpty())
+	{
+		top--;
+		return mASSiv[top + 1];
+	}
+	else
+	{
+		cout << "Р СЏРґРѕРє РІР¶Рµ РїСѓСЃС‚РёР№\n";
+	}
+
+}
+
+bool isEmpty()
+{
+	return top == 0;
+}
+
+bool isFull()
+{
+	return SIZE == top;
+
+}
+
+// additional function
+
+void print()
+{
+	for (int i = 0; i < top; i++)
+		cout << mASSiv[i]<<"\n";
 
 }
